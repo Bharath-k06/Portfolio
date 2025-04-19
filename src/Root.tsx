db933d5e28d3ebe00ecf0a03a0d1feee4c0a4f3e
Bharath-k06 <kumarluciferb57@gmail.com>
2025-04-19 15:11:07 +0530

    routing issue while deployment

--- src/Root.tsx
+++ src/Root.tsx
@@ -13,7 +13,7 @@ const Root = () => {
       <ParticleBackground/>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/Portfolio" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/project" element={<Project />} />
